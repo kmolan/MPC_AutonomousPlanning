a5b2f6a932f2b89f2536fa0d614ec3d53c84adf2
kmolan <anmolkathail@gmail.com>
2019-12-02 10:21:39 -0500

    refactoring

--- cmake-build-debug/catkin_generated/installspace/_setup_util.py
+++ cmake-build-debug/catkin_generated/installspace/_setup_util.py
@@ -270,7 +270,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = ''.split(';')
+            CMAKE_PREFIX_PATH = '/home/anmolk/mpc_ws/devel;/opt/ros/melodic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
